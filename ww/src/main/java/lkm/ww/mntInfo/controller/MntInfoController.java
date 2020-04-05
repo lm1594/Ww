0a38dba0d889ddf277a1ab44e5cfafc14970820a
이경민 <@DESKTOP-B8C9N61>
2020-04-05 15:03:14 +0900

    메인 페이지 개발

--- ww/src/main/java/lkm/ww/mntInfo/controller/MntInfoController.java
+++ ww/src/main/java/lkm/ww/mntInfo/controller/MntInfoController.java
@@ -1,6 +1,16 @@
 package lkm.ww.mntInfo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import lkm.ww.comn.util.StringUtil;
+import lkm.ww.mntInfo.domain.MntInfoVO;
+import lkm.ww.mntInfo.service.MntInfoService;
 
 /**
  * 산정보 컨트롤러
@@ -8,6 +18,32 @@ import org.springframework.stereotype.Controller;
  * @since 2020.04.05
  */
 @Controller
+@RequestMapping(value = "/ww/mntInfo")
 public class MntInfoController {
 	
+	@Autowired
+	private MntInfoService mntInfoService;
+	
+	/**
+	 * 산정보 목록 조회 화면
+	 * @param MntInfoVO
+	 * @return ModelAndView
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/mntInfoList.do")
+	public ModelAndView mntInfoList(HttpServletRequest request, @ModelAttribute("pvo") MntInfoVO pvo) throws Exception {
+		ModelAndView mav = new ModelAndView("ww/mntInfo/mntInfoList");
+		
+		// 페이지 수
+		if(StringUtil.isNullOrBlank(pvo.getPageNo())) {
+			pvo.setPageNo("1");
+		}
+		
+		// 페이지 로우 수
+		if(StringUtil.isNullOrBlank(pvo.getNumOfRows())) {
+			pvo.setNumOfRows("10");	
+		}
+		
+		return mav;
+	}
 }
---
+++ ww/src/main/webapp/WEB-INF/jsp/ww/mntInfo/mntInfoList.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ include file="/view/layout/taglib-include.jsp"%>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		
+	});
+</script>
+
+<section class="wrapper style1 align-center">
+	<div class="content align-center">
+		<span class="image fit">
+			<img src="/images/ww/ww_image.png" ondblclick="location.reload();" alt="오디!오디?" />
+		</span>
+		<h1>산 이름 조회하기</h1>
+		<ul class="actions stacked">
+			<div class="fields">
+				<div class="field" style="margin:5px;">
+					<input type="text" name="name" id="name" value="">
+				</div>
+			</div>
+			<li>
+				<a href="#" class="button primary icon solid fa-search">
+					<font style="vertical-align: inherit;">
+						<font style="vertical-align: inherit;">조회</font>
+					</font>
+				</a>
+			</li>
+		</ul>
+	</div>
+</section>
